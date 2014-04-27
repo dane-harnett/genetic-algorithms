a3b8ea3c6e7718b97255e4302fe7caa2264a7a88
Dane Harnett <dane_harnett@flightcentre.com>
2014-04-27 19:30:34 +1000

    Adding comments

--- calculate-highest-number.js
+++ calculate-highest-number.js
@@ -1,3 +1,10 @@
+// each "gene" is coded by a letter of the alphabet
+// each gene is basically a function that takes one input
+// and returns an output
+//
+// in this example we take a number as an input and perform
+// an expression on it, such as add one, minus one, sqrt, divide by 2
+// the "z" gene is a pass thru gene that does nothing.
 var genes = {
   a: function(input) {
     return input + 1;
@@ -17,28 +24,47 @@ var genes = {
   }
 };
 
+// an array of all the different possible codes
+// probably could derive this from the genes array
 var alphabet = ['a', 'b', 'c', 'd', 'z'];
 
+
+// Class Algo
+// represents an algorithm
+// constructed with a DNA string to represent the algorithm it performs
+// initialize the result and fitness
 function Algo(dna) {
   this.dna = dna;
   this.result = 0;
   this.fitness = null;
 }
+// get the DNA string
 Algo.prototype.getDna = function() {
   return this.dna;
 };
+// execute the algorithm and set the "result" member variable to the
+// final output
 Algo.prototype.execute = function() {
+  // loop through each char in the dna string
   for (var i = this.dna.length - 1; i >= 0; i--) {
+    // set the result to the return value of this genes function
     this.result = genes[this.dna[i]](this.result);
   }
 };
+// get the result
 Algo.prototype.getResult = function() {
   return this.result;
 };
+// reset the algorithm to be run again
 Algo.prototype.reset = function() {
   this.result = 0;
   this.fitness = null;
 };
+// produce an offspring algorithm by
+// taking another algorithm as a param
+// creating a new DNA string using both the parents
+// DNA strings as a source and choosing the corresponding gene
+// on a 50-50 basis
 Algo.prototype.mate = function(otherAlgo) {
   var offspringDna = '';
   for (var i = this.getDna().length - 1; i >= 0; i--) {
@@ -47,14 +73,15 @@ Algo.prototype.mate = function(otherAlgo) {
   return new Algo(offspringDna);
 };
 
-
+// generate a number of random algorithms of a specific size
 function generateAlgos(numAlgos, genomeSize) {
-  var algos = [];
-  var randomDna;
+  var algos = [],
+    randomDna;
   for (var i = 0; i < numAlgos; i++) {
     randomDna = '';
 
     for (var j = genomeSize - 1; j >= 0; j--) {
+      // add a random gene to the DNA string
       randomDna += alphabet[Math.floor(Math.random() * alphabet.length)];
     }
 
@@ -64,6 +91,7 @@ function generateAlgos(numAlgos, genomeSize) {
   return algos;
 }
 
+// reset and execute all the algos in the passed in array
 function executeAlgos(algos) {
   for (var i = algos.length - 1; i >= 0; i--) {
     algos[i].reset();
@@ -71,7 +99,10 @@ function executeAlgos(algos) {
   }
 }
 
-// the goal is to score the highest result
+// the calculateFitness function is to determine the fittest
+// algos, it takes an array of executed algos and sorts them
+// in order of fitness, fittest first.
+// this specific goal is to score the highest result
 function calculateFitness(algos) {
   algos.sort(function(a, b) {
     return a.getResult() > b.getResult();
@@ -81,6 +112,7 @@ function calculateFitness(algos) {
   }
 }
 
+// display the results of each algo
 function display(algos) {
   calculateFitness(algos);
   for (var i = algos.length - 1; i >= 0; i--) {
@@ -90,6 +122,7 @@ function display(algos) {
 
 // assume fittest are first
 // half the algos die
+// remove the last half of the array and return the result
 function naturalSelection(algos) {
   return algos.splice(0, Math.ceil(algos.length / 2));
 }
@@ -97,8 +130,11 @@ function naturalSelection(algos) {
 // double the number of algos by mating
 // fittest with each of the remaining
 function reproduce(algos) {
-  var offspring = [];
-  var mate;
+  var offspring = [],
+    mate;
+  // loop through all the algos starting with the last one,
+  // mate it with the algo before it in the array
+  // mate the first algo with the last algo
   for (var i = algos.length - 1; i >= 0; i--) {
     mate = i == 0 ? algos[algos.length - 1] : algos[i - 1];
     offspring.push(algos[i].mate(mate));
@@ -106,6 +142,9 @@ function reproduce(algos) {
   return algos.concat(offspring);
 }
 
+// generate some algos
+// execute the algos 100 times,
+// evolving after each generation
 function main() {
   var algos = generateAlgos(10, 6);
 
